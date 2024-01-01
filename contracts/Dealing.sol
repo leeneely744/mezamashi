ba43cb52d08545e644f4a9cb249e65310af12297
kohei sano <nlee9337@gmail.com>
2024-01-01 16:51:20 +0900

    add test

--- contracts/Dealing.sol
+++ contracts/Dealing.sol
@@ -2,12 +2,22 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Dealing {
-  string defaultMessage;
-  constructor() public {
-    defaultMessage = "Hello World!";
+  // This is `storage` variable. It's stored on the blockchain.
+  // So gas cost is required when you read/write this variable.
+  string message;
+
+  event Set(address sender, string newMessage);
+
+  constructor() {
+    message = "Hello World!";
   }
 
   function getMessage() public view returns (string memory) {
-    return defaultMessage;
+    return message;
+  }
+
+  function setMessage(string memory newMessage) public {
+    message = newMessage;
+    emit Set(msg.sender, newMessage);
   }
 }
--- helloWorld.js
+++ helloWorld.js
@@ -1,28 +1,80 @@
 export default function () {
     var abi = [
         {
-            "constant": true,
-            "inputs": [],
-            "name": "getMessage",
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "string"
-                }
-            ],
-            "payable": false,
-            "stateMutability": "pure",
-            "type": "function"
+          "inputs": [],
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "internalType": "address",
+              "name": "sender",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "string",
+              "name": "newMessage",
+              "type": "string"
+            }
+          ],
+          "name": "Set",
+          "type": "event"
+        },
+        {
+          "inputs": [],
+          "name": "getMessage",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "newMessage",
+              "type": "string"
+            }
+          ],
+          "name": "setMessage",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
         }
-    ];
+      ];
   
-    var address = "0x13616FAE16A6f272042422a9710242385993C608";
+    var address = "0xFB7495778B895aD9F846E02AE2835Daea18BE5D1";
   
     const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
   
     var contract = new web3.eth.Contract(abi, address);
-    contract.methods.getMessage().call(function (err, res) {
-      document.getElementById("contract-result").textContent = res;
-      console.log(res);
+    contract.methods.getMessage().call().then(function (result) {
+        console.log(result);
+        document.getElementById("contract-result").textContent = result;
     });
+    
+    // var eventContract = new web3.eth.Contract(abi, address);
+    // eventContract.events.Set({})
+    // .on('data', function(event){
+    //   console.log("event callback.");
+    //   document.getElementById("contract-result").textContent = event.returnValues.newWord;
+    // })
+    // .on('error', console.error);
+
+    // document.getElementById("button_set").onclick = () => {
+    //     let time = Math.floor(Date.now() / 1000);
+    //     console.log(time);
+    //     contract.methods.setMessage(time.toString(), function (err, res) {
+    //         console.log(res);
+    //     }
+    // )};
   }
