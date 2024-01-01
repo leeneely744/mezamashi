124d9ff326e20653b50c8a3e691be7d487840eec
kohei sano <nlee9337@gmail.com>
2024-01-01 19:29:01 +0900

    add startEventListening()

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,5 @@
 export default function () {
-  var abi = [
+  let abi = [
     {
       "inputs": [],
       "stateMutability": "nonpayable",
@@ -52,15 +52,15 @@ export default function () {
     }
   ];
   
-  var address = "0xFB7495778B895aD9F846E02AE2835Daea18BE5D1";
+  let address = "0xFB7495778B895aD9F846E02AE2835Daea18BE5D1";
+  let latestBlock = 0;
   
   const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
   
-  var contract = new web3.eth.Contract(abi, address);
+  let contract = new web3.eth.Contract(abi, address);
   contract.methods.getMessage().call().then(function (result) {
       console.log(result);
-      let date = new Date(result * 1000);
-      document.getElementById("contract-result").textContent = date.toLocaleString();
+      document.getElementById("contract-result").textContent = dateText(result);
   });
 
   document.getElementById("button_set").onclick = () => {
@@ -74,7 +74,30 @@ export default function () {
       })
       .on("receipt", function (receipt) {
           console.log(receipt);
+          let blockNumber = receipt.events.Set.blockNumber;
+          startEventListening(blockNumber);
       });
     });
   };
+
+  function startEventListening(fromBlockNumber) {
+    contract.events.Set({
+      fromBlock: fromBlockNumber
+    }, function (error, event) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(event);
+      }
+    })
+    .on("data", function (event) {
+      console.log('event: ', event);
+      document.getElementById("contract-result").textContent = dateText(event.returnValues.newMessage);
+    });
+  }
+
+  function dateText(milliseconds) {
+    let date = new Date(milliseconds * 1000);
+    return date.toLocaleString();
+  }
 }
