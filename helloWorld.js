f66dd9fd226471cc66959a02c588805735004fe9
kohei sano <nlee9337@gmail.com>
2024-01-01 18:15:53 +0900

    send()

--- helloWorld.js
+++ helloWorld.js
@@ -1,80 +1,79 @@
 export default function () {
-    var abi = [
+  var abi = [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
         {
-          "inputs": [],
-          "stateMutability": "nonpayable",
-          "type": "constructor"
+          "indexed": false,
+          "internalType": "address",
+          "name": "sender",
+          "type": "address"
         },
         {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": false,
-              "internalType": "address",
-              "name": "sender",
-              "type": "address"
-            },
-            {
-              "indexed": false,
-              "internalType": "string",
-              "name": "newMessage",
-              "type": "string"
-            }
-          ],
-          "name": "Set",
-          "type": "event"
-        },
-        {
-          "inputs": [],
-          "name": "getMessage",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "string",
-              "name": "newMessage",
-              "type": "string"
-            }
-          ],
-          "name": "setMessage",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
+          "indexed": false,
+          "internalType": "string",
+          "name": "newMessage",
+          "type": "string"
         }
-      ];
+      ],
+      "name": "Set",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "getMessage",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "newMessage",
+          "type": "string"
+        }
+      ],
+      "name": "setMessage",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
   
-    var address = "0xFB7495778B895aD9F846E02AE2835Daea18BE5D1";
+  var address = "0xFB7495778B895aD9F846E02AE2835Daea18BE5D1";
   
-    const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
+  const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
   
-    var contract = new web3.eth.Contract(abi, address);
-    contract.methods.getMessage().call().then(function (result) {
-        console.log(result);
-        document.getElementById("contract-result").textContent = result;
-    });
-    
-    // var eventContract = new web3.eth.Contract(abi, address);
-    // eventContract.events.Set({})
-    // .on('data', function(event){
-    //   console.log("event callback.");
-    //   document.getElementById("contract-result").textContent = event.returnValues.newWord;
-    // })
-    // .on('error', console.error);
+  var contract = new web3.eth.Contract(abi, address);
+  contract.methods.getMessage().call().then(function (result) {
+      console.log(result);
+      document.getElementById("contract-result").textContent = result;
+  });
 
-    // document.getElementById("button_set").onclick = () => {
-    //     let time = Math.floor(Date.now() / 1000);
-    //     console.log(time);
-    //     contract.methods.setMessage(time.toString(), function (err, res) {
-    //         console.log(res);
-    //     }
-    // )};
-  }
+  document.getElementById("button_set").onclick = () => {
+    ethereum.request({ method: 'eth_requestAccounts' })
+    .then((accounts) => {
+      let time = Math.floor(Date.now() / 1000);
+      console.log(time);
+      const from_address = document.getElementById("from-address").value;
+      contract.methods.setMessage(time.toString()).send({
+          from: from_address
+      })
+      .on("receipt", function (receipt) {
+          console.log(receipt);
+      });
+    });
+  };
+}
